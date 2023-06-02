c76d345fc6951a106a0d7c939f51c9b291450529
aceppaluni <113948612+aceppaluni@users.noreply.github.com>
2023-06-02 15:16:33 -0400

    Add files via upload

---
+++ Basketball Counter/index.js
@@ -0,0 +1,43 @@
+let num = 0
+let homePoint = document.getElementById("homePoint")
+let guestPoint = document.getElementById("guestPoint")
+
+function addOne() {
+    let result = num += 1
+    homePoint.innerText = result
+}
+function foulHome() {
+    let result = num -= 1
+    homePoint.innerText = result
+}
+function addTwo() {
+    let result = num += 2
+    homePoint.innerText = result
+}
+function addThree() {
+    let result = num += 3
+    homePoint.innerText = result
+}
+//////////////////////////
+
+function addOneGuest() {
+    let result = num += 1
+    guestPoint.innerText = result
+}
+function addTwoGuest() {
+    let result = num += 2
+    guestPoint.innerText = result
+}
+function addThreeGuest() {
+    let result = num += 3
+    guestPoint.innerText = result
+}
+function foulGuest() {
+    let result = num -= 1
+    guestPoint.innerText = result
+}
+function newGame(){
+    guestPoint.innerText = 0
+    homePoint.innerText = 0
+    sum = 0
+}
